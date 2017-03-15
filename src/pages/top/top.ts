6b347e5bbbb5b579ae1bbdcef39b88ed70aec122
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-03-14 20:07:17 -0500

    add ion-searchbar

--- src/pages/top/top.ts
+++ src/pages/top/top.ts
@@ -1,22 +1,40 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/*
-  Generated class for the Top page.
+import { AlbumsService } from '../../providers/albums-service';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+
 @Component({
   selector: 'page-top',
   templateUrl: 'top.html'
 })
 export class TopPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  query: string = '';
+
+  albums: any[] = [];
+  albumsCopy: any[] = [];
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public albumsService: AlbumsService
+  ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad TopPage');
+    this.albums = this.albumsService.getALL();
+    this.albumsCopy = this.albums;
+  }
+
+  onInput(){
+    this.albums = this.albumsCopy;
+    let val = this.query;
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.albums = this.albums.filter((album) => {
+        return (album.title.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
 }
